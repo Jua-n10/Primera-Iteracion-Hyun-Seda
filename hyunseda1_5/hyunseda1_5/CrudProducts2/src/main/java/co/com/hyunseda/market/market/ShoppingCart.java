3286f2a5232766377e461c2b85a143d847fd04b6
Jua-n10 <sebastianpisso00272@gmail.com>
2024-03-18 15:06:01 -0500

    Subiendo Archivo Hyunseda

---
+++ hyunseda1_5/hyunseda1_5/CrudProducts2/src/main/java/co/com/hyunseda/market/market/ShoppingCart.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.com.hyunseda.market.market;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Felipe Castro
+ */
+public class ShoppingCart {
+    private List<ItemShoppingCart> items;
+    //private List<Observador> observadores;
+
+    public ShoppingCart() {
+        this.items = new ArrayList<>();
+        //this.observadores = new ArrayList<>();
+    }
+
+    public List<ItemShoppingCart> getItems() {
+        return items;
+    }
+    
+    public void addItem(ItemShoppingCart item){
+        items.add(item);
+    }
+}
